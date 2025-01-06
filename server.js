554fa752900ad0c2766f16a0f5ce389f469d1fa7
mariaegydio <egydiomj@gmail.com>
2025-01-06 17:36:00 -0300

    feat: criar servidor inicial com rota básica

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Olá, a API está funcionando');
+});
+
+app.listen(port, ()=> {
+    console.log(`Servidor rodando na porta ${port}`)
+});
