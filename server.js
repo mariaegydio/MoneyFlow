126aa0d5ad4150732bbcc916a293039e8564063f
mariaegydio <egydiomj@gmail.com>
2025-01-06 17:54:33 -0300

    Criar esqueleto das rotas

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -10,4 +12,24 @@ app.get('/', (req, res) => {
 
 app.listen(port, ()=> {
     console.log(`Servidor rodando na porta ${port}`)
+});
+
+app.get('/categorias', (req, res) => {
+    res.send('Listar todas as categorias');
+});
+
+app.post('/categorias', (req,res)=>{
+    res.send('Adicionar uma nova categoria');
+});
+
+app.get('/transacoes', (req, res) => {
+    res.send('Listar todas as transações');
+});
+
+app.post('/transacoes', (req, res) => {
+    res.send('Adicionar uma nova transação');
+});
+
+app.get('/saldo', (req, res) => {
+    res.send('Obter saldo atual');
 });
