c5917ff714f1227d283b145a4f83220c7ff8b4d8
mariaegydio <egydiomj@gmail.com>
2025-01-08 15:47:09 -0300

    Atualizando código

--- server.js
+++ server.js
@@ -1,31 +1,73 @@
 const express = require('express');
+const bodyParser = require('bodyParser');
+const sql = require('mssql');
 
 const app = express();
-
-app.use(express.json());
-
 const port = 3000;
 
+app.use(bodyParser.json());
+
+const dbConfig = {
+    user: 'seu-uruario',
+    password: 'sua_senha',
+    server: 'seu_servidor',
+    database: 'MoneyFlow',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    },
+};
+
+// Verifica se a API está funcionando.
 app.get('/', (req, res) => {
     res.send('Olá, a API está funcionando');
 });
 
-app.listen(port, ()=> {
-    console.log(`Servidor rodando na porta ${port}`)
+// Rota para listar categorias.
+app.get('/categorias', async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request().query('SELECT * FROM Categorias');
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        console.error('Erro ao listar categorias:', err);
+        res.status(500).send('Erro no servidor.');
+    }
 });
 
-app.get('/categorias', (req, res) => {
-    res.send('Listar todas as categorias');
+// Rota para adicionar novas categorias
+app.post('/categorias', async (req,res) => {
+    const { nome } = req.body;
+
+    if (!nome) {
+        return res.status(400).send('O nome da categoria é obrigatorio.');
+    }
+
+    try {
+        const pool = await sql.connect(dbConfig);
+        await pool.request()
+        .input('nome', sql.VarChar(50), nome)
+        .query('INSERT INTO Categorias (Nome) VALUES (@nome)');
+        res.status(201).send('Categoria adicionada com sucesso!');
+    } catch (err) {
+        console.error('Erro ao inserir categoria:', err);
+        res.status(500).send('Erro no servidor.');
+    }
 });
 
-app.post('/categorias', (req,res)=>{
-    res.send('Adicionar uma nova categoria');
+// Rota para listar todas as transações.
+app.get('/transacoes', async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConfig);
+        const result = await pool.request().query('SELECT * FROM Transacoes');
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        console.error('Erro ao listar transações:', err);
+        res.status(500).send('Erro no servidor.');
+    }
 });
 
-app.get('/transacoes', (req, res) => {
-    res.send('Listar todas as transações');
-});
-
+// Rota para adicionar nova transação.
 app.post('/transacoes', (req, res) => {
     res.send('Adicionar uma nova transação');
 });
@@ -33,3 +75,7 @@ app.post('/transacoes', (req, res) => {
 app.get('/saldo', (req, res) => {
     res.send('Obter saldo atual');
 }); 
+
+app.listen(port, ()=> {
+    console.log(`Servidor rodando na porta ${port}`)
+});
